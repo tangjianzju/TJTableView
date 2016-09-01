bd61d2867277cff515d688828547765040d9f059
tangjian <tangjian@xiaomi.com>
2016-09-01 20:26:30 +0800

    test

---
+++ TJTableViewExample/TJTableViewExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TJTableViewExample
+//
+//  Created by 唐健 on 16/9/1.
+//  Copyright © 2016年 唐健. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TJTableViewExample/TJTableViewExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TJTableViewExample
+//
+//  Created by 唐健 on 16/9/1.
+//  Copyright © 2016年 唐健. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
